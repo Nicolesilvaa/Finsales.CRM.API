97911a30f288173aefb3c4674088f9fa8c584da3
Nicole Silva <nicolesilva8144@gmail.com>
2026-01-25 11:07:39 -0300

    Feat: Completando classe de domínio

--- src/main/java/com/java/Finsales/CRM/API/domain/model/Cliente.java
+++ src/main/java/com/java/Finsales/CRM/API/domain/model/Cliente.java
@@ -20,5 +20,27 @@ public class Cliente {
     private LocalDateTime dataCriacao;
     private LocalDateTime ultimaAtualizacao;
 
+    public Cliente(Long id, String nome, String documento, String email, String telefone, TipoCliente tipoCliente, EstadoCliente status, LocalDateTime dataCriacao, LocalDateTime ultimaAtualizacao) {
+        this.id = id;
+        this.nome = nome;
+        this.documento = documento;
+        this.email = email;
+        this.telefone = telefone;
+        this.tipoCliente = tipoCliente;
+        this.status = status;
+        this.dataCriacao = dataCriacao;
+        this.ultimaAtualizacao = ultimaAtualizacao;
+    }
+
+    public void atualizarContato(String email, String telefone) {
+        this.email = email;
+        this.telefone = telefone;
+        this.ultimaAtualizacao = LocalDateTime.now();
+    }
+
+    public void inativar() {
+        this.status = EstadoCliente.INATIVO;
+        this.ultimaAtualizacao = LocalDateTime.now();
+    }
 
 }
