5dfc9ff0e0568a8ea4db1376a65f4c2178188c28
Nicole Silva <nicolesilva8144@gmail.com>
2026-01-25 10:47:57 -0300

    Feat: Criando Controller cliente

---
+++ src/main/java/com/java/Finsales/CRM/API/controller/ClienteController.java
@@ -0,0 +1,29 @@
+package com.java.Finsales.CRM.API.controller;
+
+
+import com.java.Finsales.CRM.API.dto.request.CreateClienteRequest;
+import com.java.Finsales.CRM.API.service.ClienteService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping
+    public responseEntity<void>criarCliente(
+            @Valid @RequestBody CreateClienteRequest request
+            ){
+        clienteService.criarCliente(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
