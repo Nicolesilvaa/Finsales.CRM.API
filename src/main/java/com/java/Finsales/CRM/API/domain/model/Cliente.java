fd089beddd783de4794a1d13f4944cf423e532e1
Nicole Silva <nicolesilva8144@gmail.com>
2026-01-26 20:28:37 -0300

    Fix: AJustando construtor

--- src/main/java/com/java/Finsales/CRM/API/domain/model/Cliente.java
+++ src/main/java/com/java/Finsales/CRM/API/domain/model/Cliente.java
@@ -20,17 +20,24 @@ public class Cliente {
     private LocalDateTime dataCriacao;
     private LocalDateTime ultimaAtualizacao;
 
-    public Cliente(Long id, String nome, String documento, String email, String telefone, TipoCliente tipoCliente, EstadoCliente status, LocalDateTime dataCriacao, LocalDateTime ultimaAtualizacao) {
-        this.id = id;
+    public Cliente(
+            String nome,
+            String documento,
+            String email,
+            String telefone,
+            TipoCliente tipoCliente
+    ) {
         this.nome = nome;
         this.documento = documento;
         this.email = email;
         this.telefone = telefone;
         this.tipoCliente = tipoCliente;
-        this.status = status;
-        this.dataCriacao = dataCriacao;
-        this.ultimaAtualizacao = ultimaAtualizacao;
+
+        this.status = EstadoCliente.ATIVO;
+        this.dataCriacao = LocalDateTime.now();
+        this.ultimaAtualizacao = LocalDateTime.now();
     }
+
 
     public void atualizarContato(String email, String telefone) {
         this.email = email;
