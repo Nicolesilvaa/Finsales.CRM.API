b55a5f3878d827775414aa1e7ee34a6f479822f4
Nicole Silva <nicolesilva8144@gmail.com>
2026-01-26 22:05:28 -0300

    Fix: Ajustando o nome do método para o sping reconhecer

--- src/main/java/com/java/Finsales/CRM/API/domain/repository/ClienteRepository.java
+++ src/main/java/com/java/Finsales/CRM/API/domain/repository/ClienteRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
-    boolean existByDocumento(String documento);
+    boolean existsByDocumento(String documento);
 }
