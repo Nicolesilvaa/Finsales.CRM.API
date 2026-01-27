72abde28d09d6ebd6c61846c0d1c6d0ffae93273
Nicole Silva <nicolesilva8144@gmail.com>
2026-01-26 21:22:40 -0300

    Feat: Adiconando método de listar clientes no controller

--- src/main/java/com/java/Finsales/CRM/API/controller/ClienteController.java
+++ src/main/java/com/java/Finsales/CRM/API/controller/ClienteController.java
@@ -1,12 +1,15 @@
 package com.java.Finsales.CRM.API.controller;
 
 
+import com.java.Finsales.CRM.API.domain.model.Cliente;
 import com.java.Finsales.CRM.API.dto.request.CreateClienteRequest;
 import com.java.Finsales.CRM.API.service.ClienteService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -26,4 +29,10 @@ public class ClienteController {
         clienteService.criarCliente(request);
         return ResponseEntity.status(HttpStatus.CREATED).build(); //201
     }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listarClientes() {
+        return ResponseEntity.ok(clienteService.listar());
+    }
+
 }
