4020720420435b07de233c0b6bf84a9c963c2c4e
Nicole Silva <nicolesilva8144@gmail.com>
2026-01-26 21:59:40 -0300

    Fix: Ajustando imports

--- src/main/java/com/java/Finsales/CRM/API/dto/request/CreateClienteRequest.java
+++ src/main/java/com/java/Finsales/CRM/API/dto/request/CreateClienteRequest.java
@@ -6,10 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
+
+
 
 @Getter
 @Setter
