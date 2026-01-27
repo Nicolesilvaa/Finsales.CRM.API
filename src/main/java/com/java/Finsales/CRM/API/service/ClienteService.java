326160bfa21a932a58993538246732d880beab5c
Nicole Silva <nicolesilva8144@gmail.com>
2026-01-26 21:23:48 -0300

    Feat: Adicionando regra de negócio no clienteService

---
+++ src/main/java/com/java/Finsales/CRM/API/service/ClienteService.java
@@ -0,0 +1,41 @@
+package com.java.Finsales.CRM.API.service;
+
+import com.java.Finsales.CRM.API.domain.model.Cliente;
+import com.java.Finsales.CRM.API.dto.request.CreateClienteRequest;
+import com.java.Finsales.CRM.API.domain.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public void criarCliente(CreateClienteRequest request){
+
+        boolean clienteExiste = clienteRepository.existByDocumento(request.getDocumento());
+
+        if(clienteExiste){
+            throw new IllegalArgumentException("Cliente já cadastrado com este documento");
+        }
+
+        Cliente cliente = new Cliente(
+                request.getNome(),
+                request.getDocumento(),
+                request.getEmail(),
+                request.getTelefone(),
+                request.getTipoCliente()
+        );
+
+        clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
+    }
+
+}
