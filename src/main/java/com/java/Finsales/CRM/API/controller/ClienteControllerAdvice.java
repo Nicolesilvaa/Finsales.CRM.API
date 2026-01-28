b6ee71a309b2993a4c3231aa64680126a5bd6992
Nicole Silva <nicolesilva8144@gmail.com>
2026-01-27 21:46:46 -0300

    Feat: Criando  controller advice

--- src/main/java/com/java/Finsales/CRM/API/controller/ClienteControllerAdvice.java
+++ src/main/java/com/java/Finsales/CRM/API/controller/ClienteControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.java.Finsales.CRM.API.controller;
 
 import com.java.Finsales.CRM.API.domain.utils.exceptions.ClienteExistenteException;
+import com.java.Finsales.CRM.API.domain.utils.exceptions.ClienteNaoEncontradoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,28 +10,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ClienteControllerAdvice {
 
-    @ControllerAdvice
-    public class ClienteControllerAdvice {
 
-        @ExceptionHandler(ClienteExistenteException.class)
-        public ResponseEntity<String> clienteExistente(ClienteExistenteException ex) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
-        }
-
-        @ExceptionHandler(ClienteNaoEncontradoException.class)
-        public ResponseEntity<String> clienteNaoEncontrado(ClienteNaoEncontradoException ex) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-        }
-
-        @ExceptionHandler(IllegalArgumentException.class)
-        public ResponseEntity<String> dadosInvalidos(IllegalArgumentException ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
-        }
-
-        @ExceptionHandler(Exception.class)
-        public ResponseEntity<String> erroGenerico(Exception ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno inesperado");
-        }
+    @ExceptionHandler(ClienteExistenteException.class)
+    public ResponseEntity<String> clienteExistente(ClienteExistenteException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
+
+    @ExceptionHandler(ClienteNaoEncontradoException.class)
+    public ResponseEntity<String> clienteNaoEncontrado(ClienteNaoEncontradoException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> dadosInvalidos(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> erroGenerico(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno inesperado");
+    }
+
 
 }
