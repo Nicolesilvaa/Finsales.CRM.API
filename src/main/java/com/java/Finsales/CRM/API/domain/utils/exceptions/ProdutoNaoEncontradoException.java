329a633ba70d4f1ac1f022930daeeb5ec6b8b067
Nicole Silva <nicolesilva8144@gmail.com>
2026-02-13 19:23:18 -0300

    Fix: AJustes exception

---
+++ src/main/java/com/java/Finsales/CRM/API/domain/utils/exceptions/ProdutoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.java.Finsales.CRM.API.domain.utils.exceptions;
+
+public class ProdutoNaoEncontradoException extends RuntimeException {
+    public ProdutoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/java/Finsales/CRM/API/domain/utils/exceptions/ProdutoNaoEnontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.java.Finsales.CRM.API.domain.utils.exceptions;
-
-public class ProdutoNaoEnontradoException extends RuntimeException {
-    public ProdutoNaoEnontradoException(String message) {
-        super(message);
-    }
-}
