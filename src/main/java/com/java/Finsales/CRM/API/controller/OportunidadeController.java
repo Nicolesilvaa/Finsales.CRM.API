dc0fa9d67512766d9fc4a5b4e72cc2348029a095
Nicole Silva <nicolesilva8144@gmail.com>
2026-02-13 20:18:32 -0300

    Feat: Finalizando estrutura da model oportunidades

---
+++ src/main/java/com/java/Finsales/CRM/API/controller/OportunidadeController.java
@@ -0,0 +1,35 @@
+package com.java.Finsales.CRM.API.controller;
+
+import com.java.Finsales.CRM.API.domain.model.Oportunidade;
+import com.java.Finsales.CRM.API.dto.request.CreateOportunidadeRequest;
+import com.java.Finsales.CRM.API.service.OportunidadeService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/oportunidades")
+public class OportunidadeController {
+
+    private final OportunidadeService oportunidadeService;
+
+    public OportunidadeController(OportunidadeService oportunidadeService) {
+        this.oportunidadeService = oportunidadeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Oportunidade> criarOportunidade(@Valid @RequestBody CreateOportunidadeRequest request){
+
+        Oportunidade oportunidade = oportunidadeService.criarOportunidade(
+                request.getClienteId(),
+                request.getProdutoId()
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(oportunidade);
+    }
+
+}
---
+++ src/main/java/com/java/Finsales/CRM/API/dto/request/CreateOportunidadeRequest.java
@@ -0,0 +1,16 @@
+package com.java.Finsales.CRM.API.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateOportunidadeRequest {
+
+    @NotNull
+    private Long clienteId;
+
+    @NotNull
+    private Long produtoId;
+}
