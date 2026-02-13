9af1cc2ed561604e663ea5d7a255a56b0dd1902a
Nicole Silva <nicolesilva8144@gmail.com>
2026-02-13 19:23:59 -0300

    Feat: Criando produto

--- src/main/java/com/java/Finsales/CRM/API/domain/model/Produto.java
+++ src/main/java/com/java/Finsales/CRM/API/domain/model/Produto.java
@@ -1,4 +1,33 @@
 package com.java.Finsales.CRM.API.domain.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@Table(name = "produto")
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String nome;
+    @Column(nullable = false)
+    private String descricao;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dataCriacao;
+
+    public Produto(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.dataCriacao = LocalDateTime.now();
+    }
 }
