9ef7dc8b0d076f98fa71438b753270d08da0335b
Nicole Silva <nicolesilva8144@gmail.com>
2026-02-13 19:50:38 -0300

    Fix: AJustando repositorys

--- src/main/java/com/java/Finsales/CRM/API/domain/repository/OportunidadeRepository.java
+++ src/main/java/com/java/Finsales/CRM/API/domain/repository/OportunidadeRepository.java
@@ -1,4 +1,9 @@
 package com.java.Finsales.CRM.API.domain.repository;
 
-public interface OportunidadeRepository {
+import com.java.Finsales.CRM.API.domain.model.Oportunidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OportunidadeRepository extends JpaRepository<Oportunidade,Long> {
 }
--- src/main/java/com/java/Finsales/CRM/API/domain/repository/ProdutoRepository.java
+++ src/main/java/com/java/Finsales/CRM/API/domain/repository/ProdutoRepository.java
@@ -1,6 +1,12 @@
 package com.java.Finsales.CRM.API.domain.repository;
 
+import com.java.Finsales.CRM.API.domain.model.Cliente;
+import com.java.Finsales.CRM.API.domain.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProdutoRepository {
-   boolean findById(Long produtoId);
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto,Long> {
+   boolean existsByNome(String nome);
+
 }
