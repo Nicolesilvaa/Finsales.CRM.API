faea53a0d9af420e08e1abbe023e591b9bbfdbc5
Nicole Silva <nicolesilva8144@gmail.com>
2026-02-15 19:58:29 -0300

    Feat: Adicioando path de tualização de status no controller de oportunidade

--- src/main/java/com/java/Finsales/CRM/API/controller/OportunidadeController.java
+++ src/main/java/com/java/Finsales/CRM/API/controller/OportunidadeController.java
@@ -2,14 +2,12 @@ package com.java.Finsales.CRM.API.controller;
 
 import com.java.Finsales.CRM.API.domain.model.Oportunidade;
 import com.java.Finsales.CRM.API.dto.request.Oportunidade.CreateOportunidadeRequest;
+import com.java.Finsales.CRM.API.dto.request.Oportunidade.UpdateStatusOportunidadeRequest;
 import com.java.Finsales.CRM.API.service.OportunidadeService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/oportunidades")
@@ -31,5 +29,13 @@ public class OportunidadeController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(oportunidade);
     }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Void> atualizarStatus(@PathVariable Long id, @Valid @RequestBody UpdateStatusOportunidadeRequest request){
+
+        oportunidadeService.mudarStatusOportunidade(id, request);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
